19402a0bfe9a25db36114aae2599a3d7cac8b604
Ryota-ika <daiouga@i.softbank.jp>
2022-10-17 23:54:44 +0900

    10.17

--- mineGameUBackUp/mineGameU/Assets/c#script/RandomMine.cs
+++ mineGameUBackUp/mineGameU/Assets/c#script/RandomMine.cs
@@ -1,62 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RandomMine : MonoBehaviour {
-    [SerializeField]
 
     public int input_mine_count = 5;
     public GameObject mine_quad;
+    public List<int> list=new List<int>();
     //public const int RANDOM_HEIGHT = 10;
     //public const int RANDOM_WIDTH = 10;
-    PlayerController player = new PlayerController( );
-    List<int> numbers = new List<int>( );
+    //PlayerController player = new PlayerController( );
 
-    public GameObject kugi;
-
-    public float xMinPosition = 0;
-    public float xMaxPosition = 10;
-    public float yMinPosition = 0;
-    public float yMaxPosition = 10;
     //bool[,] aa=new bool[ width, height ];//const プログラム中変えることができない数値 static 実行中いかなるタイミングにも存在する変数
     // Start is called before the first frame update
 
     void Awake( ) {
-        //    for( int i = 0; i < input_mine_count; i++ ) {
-        //        numbers.Add( i );
-        //    }
-        //    while( numbers.Count > 0 ) {
-        //        int rand_x = Random.Range( 0, 10 );
-        //        int rand_y = Random.Range( 0, 10 );
-        //        if( !( rand_x == 0 && rand_y == 9 ) && !( rand_x == 9 && rand_y == 0 ) ) {
-        //            Instantiate( mine_quad, new Vector3( rand_x, rand_y, -0.01f ), Quaternion.identity );
-        //        }
-        //        numbers.RemoveAt( rand_x );
-        //        numbers.RemoveAt( rand_y );
-        //    }
+        for( int i = 0; i < input_mine_count; i++ ) {
+            list.Add( i );
+        }
+        while( list.Count > 0 ) {
+            
+            int rand_x = Random.Range( 0, 10 );
+            int rand_y = Random.Range( 0, 10 );
+            Vector3 random_pos= new Vector3( rand_x, rand_y, -0.01f );
+            if( !( rand_x == 0 && rand_y == 9 ) && !( rand_x == 9 && rand_y == 0 ) ) {
+                Instantiate(mine_quad, random_pos, Quaternion.identity );
+            }
+            //list.Contains(rand_x);
+            //list.Remove(rand_x );
+        }
         //for( int i = 0; i < input_mine_count; i++ ) {
-        //    int rand_x=Random.Range(0,10);
-        //    int rand_y=Random.Range(0,10);
-        //    if( !(rand_x==0&&rand_y==9)&&!(rand_x==9&&rand_y==0) ) {
-        //        Instantiate(mine_quad,new Vector3(rand_x,rand_y,-0.01f),Quaternion.identity);
+        //    list.Add(i);
+        //    int rand_x = Random.Range( 0, 10 );
+        //    int rand_y = Random.Range( 0, 10 );
+        //    if( !( rand_x == 0 && rand_y == 9 ) && !( rand_x == 9 && rand_y == 0 ) ) {
+        //        Instantiate( mine_quad, new Vector3( rand_x, rand_y, -0.01f ), Quaternion.identity );
         //    }
         //}
-        for( int i = 0; i < input_mine_count; i++ ) {
-            GameObject UnmovableBox = Instantiate( kugi );
-            UnmovableBox.transform.position = GetRandomPosition( );
+        //    for( int i = 0; i < input_mine_count; i++ ) {
+        //        GameObject UnmovableBox = Instantiate( kugi );
+        //        UnmovableBox.transform.position = GetRandomPosition( );
+        //    }
+
+        //}
+
+        //private Vector3 GetRandomPosition( ) {
+        //    float x = Random.Range( xMinPosition, xMaxPosition );
+        //    float y = Random.Range( yMinPosition, yMaxPosition );
+        //    if( !( x == 0 && y == 9 ) && !( x == 9 && y == 0 ) ) {
+
+        //    }
+        //        return new Vector3( x, y, -0.01f );
+
         }
-
-    }
-
-    private Vector3 GetRandomPosition( ) {
-        float x = Random.Range( xMinPosition, xMaxPosition );
-        float y = Random.Range( yMinPosition, yMaxPosition );
-        if( !( x == 0 && y == 9 ) && !( x == 9 && y == 0 ) ) {
-
-        }
-            return new Vector3( x, y, -0.01f );
-    }
-    void Start( ) {
+        void Start( ) {
 
     }
 
